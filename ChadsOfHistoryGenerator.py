b731bf976997f233edae69cb4d7deadd580aae06
Poki <51330236+PokiDokika@users.noreply.github.com>
2021-10-06 07:03:28 +0300

    Update ChadsOfHistoryGenerator.py

--- ChadsOfHistoryGenerator.py
+++ ChadsOfHistoryGenerator.py
@@ -1,4 +1,14 @@
-import random
+import random, urllib.request
 lines = open('Chads.txt').read().splitlines()
 myline =random.choice(lines)
 print(myline)
+
+
+# Fetch wikipedia page
+try:
+  link = "https://en.wikipedia.org/wiki/"+myline.replace(" ","_")
+  page = urllib.request.urlopen(link)
+  if(page.getcode() == 200):
+    print("Read more about "+myline+" at Wikipedia on: "+link)
+except:
+    print("No Wikipedia page found.")
